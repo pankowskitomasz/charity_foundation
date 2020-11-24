6fe83a1ad5561cdd747a680269019d1b0f8ba63d
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-11-24 17:09:35 +0100

    finished frontend

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+$("#index-s1-owl").owlCarousel({loop:true,items:1});
+$("#gallery-s2-owl,#gallery-s3-owl,#gallery-s4-owl").owlCarousel({
+    loop:true,
+    responsive:{
+        0:{items:1},
+        640:{items:2},
+        1024:{items:3}
+    }
+});
